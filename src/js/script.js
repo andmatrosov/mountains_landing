d3ad7ddac35099abd5ac8b240fb5465bdc0f1bb4
Andrew <matrosow.andrew@yandex.ru>
2021-03-28 13:19:38 +0300

    Доверстал все блоки, приступил адаптиву

--- src/js/script.js
+++ src/js/script.js
@@ -6,10 +6,27 @@ window.addEventListener('scroll', () => {
   layers.forEach((e) => {
     if (e.hasAttribute('data-speed')) {
       speed = e.getAttribute('data-speed');
-      console.log(speed);
       let yPos = -(pageTop * speed) / 100;
-      console.log(pageTop, yPos);
       e.setAttribute('style', `transform: translate3d(0px, ${yPos}px, 0px)`);
     }
   });
+});
+
+window.addEventListener('scroll', () => {
+  let scrollDistance = window.scrollY;
+  let menuItem = document.querySelectorAll('.scroll-nav__item');
+
+  document
+    .querySelectorAll('.home, .main-content__item')
+    .forEach((el, index) => {
+      if (el.offsetTop <= scrollDistance) {
+        menuItem.forEach((el) => {
+          if (el.classList.contains('active')) {
+            el.classList.remove('active');
+          }
+        });
+        menuItem[index].classList.add('active');
+        console.log('Переключение');
+      }
+    });
 });
